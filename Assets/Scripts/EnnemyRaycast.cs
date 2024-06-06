d1aa87114a3d3f1f7eae1cb95bb9ee9beea2e657
Zaaadig <z_loyer@stu-lisaa.com>
2024-06-06 12:47:27 +0200

    Début sur les raycast
    j'ai un raycast qui récupère l'info de ce qu'il touche en fonction du layer.
    
    Maintenant faut utiliser l'info en fonction du contexte (ennemy ou wall) et s'en servir différemment

--- Assets/Scripts/EnnemyRaycast.cs
+++ Assets/Scripts/EnnemyRaycast.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnnemyRaycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    public float raycastDistance = 100f;
+    public LayerMask hitReturn;
+    public KeyCode scan = KeyCode.E;
+
+    private Ray ray;
+
+    private void Update()
     {
-        
+        MyInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MyInput()
     {
-        
+        if (Input.GetKeyDown(scan))
+        {
+            RaycastShoot();
+        }
+    }
+    private void RaycastShoot()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, raycastDistance, hitReturn, QueryTriggerInteraction.Ignore))
+        {
+            Debug.Log(hit.collider.gameObject.name + "was hit!");
+        }
     }
 }
